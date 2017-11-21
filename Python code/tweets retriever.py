de05b2ae3ae5b55c7e851529c46a9442463fa934
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2017-11-21 12:37:38 +0100

    working retriever

---
+++ Python code/tweets retriever.py
@@ -0,0 +1,43 @@
+client_key = 'U0zHy6gQlsMpQumbEgHWXb7nm'
+client_secret = 'CbsDAod2sb31pLqVzBctFdoNMsTQPUZTm1ZInaXm62gmDP7J7I'
+
+import base64
+
+key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
+b64_encoded_key = base64.b64encode(key_secret)
+b64_encoded_key = b64_encoded_key.decode('ascii')
+
+import requests
+
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+access_token = auth_resp.json()['access_token']
+
+search_headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+
+search_params = {
+    'q': 'Jumbo en Plus in Delft verkopen spruitjes los',
+    'result_type': 'recent',
+    'count': 1
+}
+
+search_url = '{}1.1/search/tweets.json?tweet_mode=extended'.format(base_url)
+
+search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+tweet_data = search_resp.json()
+for x in tweet_data['statuses']:
+    print(x['full_text'] + '\n')
