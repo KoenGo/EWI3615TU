6dbc1ca8d039571f500978f08eff6cbdf4032e87
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2017-11-23 14:10:54 +0100

    change of input questions

--- Python code/main.py
+++ Python code/main.py
@@ -1,6 +1,6 @@
 from tweets_retriever import tweets
-search_text = input("What do you want to search for?")
-number_of_tweets = input("How many tweets?")
+search_text = input("For what search term do you want the sentiment?")
+number_of_tweets = input("How many tweets do you want to use?")
 list_of_tweets = tweets().get(search_text, number_of_tweets)
 
 from sentiment_analyzer import sentiment
