ea847ac27c40a7e63cd37213fd24f57f3d520da9
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2017-11-23 12:56:26 +0100

    retriever implemented in main.py and fixed problem retrieving retweets

--- Python code/main.py
+++ Python code/main.py
@@ -1,1 +1,6 @@
-print("Test")
+from tweets_retriever import tweets
+search_text = input("What do you want to search for?")
+number_of_tweets = input("How many tweets?")
+list_of_tweets = tweets().get(search_text, number_of_tweets)
+for tweet in list_of_tweets:
+    print(tweet)
