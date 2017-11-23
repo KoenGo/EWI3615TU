367f952de5fb07fb9d7efe6cf27cf22a7fb31b63
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2017-11-23 13:31:38 +0100

    implemented analyzer in main

--- Python code/main.py
+++ Python code/main.py
@@ -2,5 +2,8 @@ from tweets_retriever import tweets
 search_text = input("What do you want to search for?")
 number_of_tweets = input("How many tweets?")
 list_of_tweets = tweets().get(search_text, number_of_tweets)
-for tweet in list_of_tweets:
-    print(tweet)
+
+from sentiment_analyzer import sentiment
+positivity = sentiment().get(list_of_tweets)
+mean_positivity = sum(positivity)/len(positivity)
+print(mean_positivity)
