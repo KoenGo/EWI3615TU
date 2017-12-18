819c942e88583d783664c2bbe6796acb995793d6
KoenGo <kgoedemondt@gmail.com>
2017-12-18 23:33:36 +0100

    created class Newspage(). Creating a Newspage object retrieves source code from cnn's 50 most recent and enables you to perform actions on them

--- Python code/webscraper.py
+++ Python code/webscraper.py
@@ -1,28 +1,71 @@
 # Retrieve nouns from titles of news reports at http://edition.cnn.com/specials/last-50-stories
 # use the nouns to find tweets and apply sentiment analysis
-# remove retweets, tweets with similiar contents
+# remove retweets, tweets with similar contents
 
 # which webscraper api to use?
 # https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
 
 import requests
 import re
-import codecs
 from textblob import TextBlob
-import bs4 as beautifulsoup
+import datetime
+from collections import Counter as counter
+import operator
 
-url  = "http://edition.cnn.com/specials/last-50-stories"
-newspage = requests.get(url)
-splitlines = re.findall("(.{100})",newspage.text)
+class Newspage():
+    def __init__(self):
+        self.url = "http://edition.cnn.com/specials/last-50-stories"
+        self.source = requests.get(self.url).text
+        self.timestamp = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
+        # Write whole source code to txt file
+        f = open('newspage.txt', 'w')
+        f.write(self.source)
+        f.close()
 
+    def __str__(self):
+        top_five = [i for i in self.most_common_nouns(5)]
+        return "News page created at: " + str(self.timestamp) + "\nWith top five: " + str(top_five)
 
-# Write content to txt file
-f = codecs.open('newspage.txt', encoding='utf-8', mode='w')
-for line in splitlines:
-    f.write(line + "\n")
-f.close()
+    # Get headlines from the source code
 
-# Extract nouns
-blob = TextBlob("Atlanta airport outage complicates insanely busy holiday travel")
-print(blob.noun_phrases)
+    def get_headlines(self, file):
+        newspage = open(file, 'rt').read()
+        headlines = re.findall('(?<=<span class="cd__headline-text">)([^<]*)', newspage)
+        return headlines
+
+    # Method for extracting nouns
+    # extract_nouns(sentence: str) -> [str]
+
+    def extract_nouns(self, sentence):
+        if isinstance(sentence, str):
+            tb_sentence = TextBlob(sentence)
+        else:
+            raise Exception("Input must be string")
+        result = []
+        nounlist = tb_sentence.noun_phrases
+        for i in nounlist:
+            result += i.split(" ")
+        return result
+
+    # Extract all returns a list of lists containing all nouns
+    # with each sublist corresponding to one headline
+
+    def extract_all(self):
+        result = []
+        for headline in self.get_headlines('newspage.txt'):
+            result.append(self.extract_nouns(headline))
+        return result
+
+    def most_common_nouns(self, amount=10):
+        """ Set amount to 0 for all nouns """
+        all_nouns = []
+        for noun_list in self.extract_all():
+            all_nouns += noun_list
+
+        noun_dict = counter(all_nouns)
+        sorted_nouns = sorted(noun_dict.items(), key=operator.itemgetter(1), reverse=True)
+        if int(amount) > 0:
+            return sorted_nouns[:amount]
+        else:
+            return sorted_nouns
