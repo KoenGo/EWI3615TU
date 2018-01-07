7da2d577f231e4b33978ccb6f4da48ad19aa67be
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2018-01-07 14:48:25 +0100

    Implemented choice for location or text

--- Python code/main.py
+++ Python code/main.py
@@ -1,8 +1,15 @@
 from tweets_retriever import tweets
 data_list = []
-search_text = input("For what search term do you want the sentiment? ")
+search_text = ""
+text_or_location = ""
+text_or_location = input("Do you want to search location of text? type l for location and t for text:")
+while text_or_location != "l" and text_or_location != "t":
+    text_or_location = input("Your input is not correct, type l for location and t for text:")
+if text_or_location == "t":
+    search_text = input("For what search term do you want the sentiment? ")
 number_of_tweets = input("How many tweets do you want to use? ")
-data_list = tweets().get(search_text, number_of_tweets, data_list)
+
+data_list = tweets().get(search_text, text_or_location, number_of_tweets, data_list)
 
 print(data_list)
 from sentiment_analyzer import sentiment
--- Python code/tweets_retriever.py
+++ Python code/tweets_retriever.py
@@ -3,7 +3,7 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from stream_listener import listener
 class tweets():
-    def get(self, search_text,number_of_tweets, data_list):
+    def get(self, search_text, text_or_location, number_of_tweets, data_list):
         client_key = 'U0zHy6gQlsMpQumbEgHWXb7nm'
         client_secret = 'CbsDAod2sb31pLqVzBctFdoNMsTQPUZTm1ZInaXm62gmDP7J7I'
         access_token = '932924142906761216-25dcDBqKSnXX2sDJKmF7MYGbuQ2EgXQ'
@@ -13,6 +13,9 @@ class tweets():
         auth = OAuthHandler(client_key, client_secret)
         auth.set_access_token(access_token, access_secret)
 
-        twitterStream = Stream(auth, listener(number_of_tweets, data_list))
-        twitterStream.filter(locations=[-124.3150818348,24.3677378899,-68.5045349598,49.1534179227])
+        twitterStream = Stream(auth, listener(number_of_tweets, data_list, text_or_location))
+        if text_or_location is "l":
+            twitterStream.filter(locations=[-124.3150818348,24.3677378899,-68.5045349598,49.1534179227])
+        else:
+            twitterStream.filter(track=[search_text])
         return data_list
