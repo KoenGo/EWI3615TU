398036b6d068bd50e7b61751ce5622695850d4fc
Koen <kgoedemondt@gmail.com>
2018-01-10 17:01:27 +0100

    implemented beautifulsoup for making pages more readable, and finding proper headline blocks

--- Python code/bs_test.py
+++ Python code/bs_test.py
@@ -8,6 +8,9 @@ source = req.text
 
 soup = bs(source,'html.parser')
 
+with open('raw_source.txt', 'wb') as file:
+    file.write(source.encode('utf-8', 'ignore'))
+
 with open('bs_source.txt', 'wb') as file:
      file.write(soup.prettify().encode('utf-8','ignore'))
 
@@ -20,17 +23,21 @@ with open('bs_strings.txt', 'wb') as file:
 #
 # read lines, write lines to txt file until regex is found
 #
+def top_stories_to_file():
+    with open('bs_source.txt', 'r', errors='ignore') as file:
+        lines = file.readlines()
+        line_iter = iter(lines)
+        line = lines[0]
+        with open('chopped_source.txt', 'w') as chopped_file:
+            while True:
+                if re.search('(\<a class="\w{6}" href="headlines/section/topic/WORLD\?ned=us" jslog="\d{5}; track: click;"\>)[^World]',line):
+                    break
+                chopped_file.write(line)
+                line = next(line_iter)
 
-with open('bs_source.txt', 'r', errors='ignore') as file:
-    lines = file.readlines()
-    line_iter = iter(lines)
-    line = lines[0]
-    with open('chopped_source.txt', 'w') as chopped_file:
-        while True:
-            if re.search('(\<a class="\w{6}" href="headlines/section/topic/WORLD\?ned=us" jslog="\d{5}; track: click;"\>)[^World]',line):
-                break
-            chopped_file.write(line)
-            line = next(line_iter)
+# def split_top_stories():
+#     with open('chopped_source.txt', 'r', errors='ignore') as file:
+#         while
 
 def get_headlines():
     newspage = open('chopped_source.txt', 'r').read()
@@ -44,3 +51,9 @@ def headlines_to_file(headlines):
 
 print(get_headlines())
 headlines_to_file(get_headlines())
+
+# <c-wiz jsrenderer="WluEBc" class="ROONz H7eG6b" jsshadow jsdata="deferred-i1" data-p="%.@.&quot;headlines&quot;,null,null,null,&quot;us&quot;]
+# " jscontroller="GILUZe" jsaction="click:cOuCgd;RI2Xre:Vtdxob;" data-node-index="0;0" jsmodel="hc6Ubd">
+# get turned into
+# <c-wiz class="ROONz H7eG6b" data-node-index="0;0" data-p='%.@."headlines",null,null,null,"us"]
+# ' jsaction="click:cOuCgd;RI2Xre:Vtdxob;" jscontroller="GILUZe" jsdata="deferred-i1" jsmodel="hc6Ubd" jsrenderer="WluEBc" jsshadow="">
