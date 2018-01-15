fdc75b312df1f6fbbb4dd253f98cd166c7aea1f3
LAPTOP-TIJMEN\Tijmendejong97 <t.w.dejong@student.tudelft.nl>
2018-01-15 14:17:04 +0100

    fixed problem in main when inputing y/n

--- Python code/main.py
+++ Python code/main.py
@@ -6,7 +6,9 @@ while datacollector.interval == 0 or datacollector.number_of_intervals == 0 or d
         datacollector.interval = float(input("Set interval in minutes: "))
         datacollector.number_of_intervals = int(input("Set number of intervals: "))
         datacollector.remove_neutral_tweets = input("Discard neutral tweets? (Y/N): ").lower()
-        if datacollector.remove_neutral_tweets != ("y" or "n"):
+        if datacollector.remove_neutral_tweets == "y" or datacollector.remove_neutral_tweets == "n":
+            pass
+        else:
             datacollector.remove_neutral_tweets = "w"
             raise ValueError
     except ValueError:
